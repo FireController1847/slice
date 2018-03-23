725e6f09325173fec0cd833a9f02bf161a55ff29
FireController1847 <usfirepilot123@gmail.com>
2018-03-22 23:49:29 -0600

    Info, Reload, and Ping Commands

---
+++ src/Commands/Utility/Ping.js
@@ -0,0 +1,15 @@
+const { Command } = require('discord-akairo');
+
+class Ping extends Command {
+  constructor() {
+    super('ping', {
+      aliases: ['ping'],
+      description: 'Gets the current speed of the WebSocket.'
+    });
+  }
+  exec(m) {
+    return m.channel.send(`The current speed of the WebSocket is ${this.client.ping.toFixed(0)}ms.`);
+  }
+}
+
+module.exports = Ping;

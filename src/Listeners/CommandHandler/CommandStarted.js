0b8db700f411d6c50964b58c5fd9fb30c9702532
FireController1847 <usfirepilot123@gmail.com>
2018-03-24 16:29:05 -0600

    Change Logger from CommandFinished to CommandStarted

--- src/Listeners/CommandHandler/CommandStarted.js
+++ src/Listeners/CommandHandler/CommandStarted.js
@@ -1,17 +1,18 @@
 const { Listener } = require('discord-akairo');
 
-class CommandHandlerCommandFinished extends Listener {
+class CommandHandlerCommandStarted extends Listener {
   constructor() {
-    super('commandHandlerCommandFinished', {
+    super('commandHandlerCommandStarted', {
       emitter: 'commandHandler',
-      event: 'commandFinished'
+      event: 'commandStarted'
     });
   }
 
   exec(m, c) {
+    if (['info', 'database'].includes(c.id)) return;
     if (m.guild) console.log(`User ${m.author.username} (${m.author.id}) issued server command ${this.client.akairoOptions.prefix}${c.id} in ${m.guild.name} (${m.guild.id}), #${m.channel.name}`);
     else console.log(`User ${m.author.username} (${m.author.id}) issued private command ${this.client.akairoOptions.prefix}${c.id}.`);
   }
 }
 
-module.exports = CommandHandlerCommandFinished;
+module.exports = CommandHandlerCommandStarted;
